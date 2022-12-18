da78b9c8f7fc9c2fd76162aaade63f29a3be04c5
flaviomarcilio <flaviomarciliodeoliveira@gmail.com>
2022-12-18 08:35:49 -0300

    Módulo 3 - VPL 04

---
+++ PDSII/Modulo3/VPL 4 - Decorador de Formas (Extra - 0,0 pts)/OpacidadeDecorator.cpp
@@ -0,0 +1,22 @@
+#include "OpacidadeDecorator.hpp"
+
+std::string OpacidadeDecorator::desenha() {
+    
+    if (this->_opacidade >= 0 && this->_opacidade <= 0.33) {
+        return FormaDecorator::desenha() + "\n- opacidade: baixa";
+    } else if (this->_opacidade > 0.33 && this->_opacidade <= 0.66) {
+        return FormaDecorator::desenha() + "\n- opacidade: média";
+    } else if (this->_opacidade > 0.66 && this->_opacidade <= 1.0) {
+        return FormaDecorator::desenha() + "\n- opacidade: alta";
+    }
+
+    return FormaDecorator::desenha();
+}
+  
+void OpacidadeDecorator::setOpacidade(double opacidade) {
+    this->_opacidade = opacidade;
+}
+
+double OpacidadeDecorator::getOpacidade() {
+    return this->_opacidade;
+}
